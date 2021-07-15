d066ce2f2a2de7697a1b0e539fb473b330fb8061
dhakadgyans <55667666+dhakadgyans@users.noreply.github.com>
2021-07-15 20:24:38 +0530

    Update HerokuController.java
    Rest call for AgeCalculator app

--- src/main/java/com/gyan/spring/auth/HerokuController.java
+++ src/main/java/com/gyan/spring/auth/HerokuController.java
@@ -3,15 +3,26 @@ package com.gyan.spring.auth;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HerokuController {
+	
+	RestTemplate rt = new RestTemplate();
 	@GetMapping("/")
 	public String gethome() {
 		return "Welcome to Heroku App";
 	}
 	@GetMapping("/name/{name}")
 	public String gethome1(@PathVariable String name) {
-		return "Welcome to Heroku App---> "+name;
+		
+		return "<h2>Hello "+name+"</h2><h3><br>Welcome to my First Heroku App.</h3>";
+	}
+	
+	@GetMapping("/name/{name}/{year}")
+	public String gethome2(@PathVariable String name, @PathVariable int year) {
+		
+		int age= rt.getForObject("https://age-calculator-app.herokuapp.com/birth-year/"+year, Integer.class);
+		return "<h2>So, "+name+" <br><br>Here is your age. "+"</h2><h1><br>"+age+" years !!!</h1>";
 	}
 }
